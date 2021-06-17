60c416c59823609b48c0ee183e32cdced9dc28d4
abuhanaan <adewunmi99@gmail.com>
2021-06-17 13:35:58 -0700

    Fixed 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * This program prints to the standard output using the puts function
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
