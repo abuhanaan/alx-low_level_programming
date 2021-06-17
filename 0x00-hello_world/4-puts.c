19bdb35ac674f17f746bfab72b80e9bebc456acd
abuhanaan <adewunmi99@gmail.com>
2021-06-17 13:20:26 -0700

    Fixed 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
-        char str1[55];
-
-        strcpy(str1, "\"Programming is like building a multilingual puzzle\n");
-
-        puts(str1);
-
-        return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
