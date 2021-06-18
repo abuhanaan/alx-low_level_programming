330747625227e2a1bde96d9c6b3c614fbfdc26b4
abuhanaan <adewunmi99@gmail.com>
2021-06-18 09:40:09 -0700

    Prints whether or not random variable is negative or positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 /**
- * main: Prints the sign of a random number to standard output
+ * main - Prints the sign of a random number to standard output
  * Return: 0
  */
 int main(void)
@@ -17,15 +17,15 @@ int main(void)
 
 	 if (n > 0)
 {
-        printf("%d is positive\n", n);
+        printf("%d is positive", n);
 }
         else if (n == 0)
 {
-        printf("%d is zero\n", n);
+        printf("%d is zero", n);
 }
         else
 {
-        printf("%d is negative\n", n);
+        printf("%d is negative", n);
 }
 
 
