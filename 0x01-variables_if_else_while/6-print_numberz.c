27d44cd5ac8d075d7ef36a5fd46c9af6f31f39bb
abuhanaan <adewunmi99@gmail.com>
2021-06-20 02:18:43 -0700

    Prints numbers from 0-9 using only putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints from 0 -9 using only putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
