ef03b87f416eead1a8916d264f2a61225077e3d7
abuhanaan <adewunmi99@gmail.com>
2021-06-20 01:45:05 -0700

    Prints from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints from 0 -9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+
+	return (0);
+}
