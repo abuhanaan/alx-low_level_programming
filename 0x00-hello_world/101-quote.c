0c29fb7eaefe613b8cd67f535b99f78b2684ecbb
abuhanaan <adewunmi99@gmail.com>
2021-06-20 04:59:58 -0700

    Prints the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints to standard without printf and puts
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *q = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+
+	putchar(int(q));
+
+	return (0);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,18 +7,11 @@
 
 int main(void)
 {
-
-	int i;
-	int long long h;
-	int long s;
-	float f;
-	char c;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned int)sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", (unsigned int)sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned int)sizeof(s));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned int)sizeof(h));
-	printf("Size of a float: %lu byte(s)\n", (unsigned int)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
 }
