51752728824771216ae9fbc3a999cee4eec91c98
abuhanaan <adewunmi99@gmail.com>
2021-06-23 05:26:35 -0700

    Prints the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _abs - Prints the absolute value of a number
+ * @n: The number to convert to absolute
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	_putchar(abs(n));
+
+	return (0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
+#include <stddef.h>
 
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
