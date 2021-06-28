2aa6d73f3072477bcb8669488a84c067631f3259
abuhanaan <adewunmi99@gmail.com>
2021-06-28 12:21:48 -0700

    Uses a pointer to manipulate an int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: The pointer that updates the int
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
