8d9bedac7099b51476ba7c812c67978848a41013
abuhanaan <adewunmi99@gmail.com>
2021-06-28 13:10:33 -0700

    Prints the lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * _strlen - Prints the length of a string
+ * @s: The string in question
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
