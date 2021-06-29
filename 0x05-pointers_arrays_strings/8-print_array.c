776a8469930fd876b3d1f390419f24e3ad040f60
abuhanaan <adewunmi99@gmail.com>
2021-06-29 08:10:30 -0700

    prints n elements of an array of integers, followed by a new line

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,8 +8,8 @@
 
 void print_array(int *a, int n)
 {
-	int i;
-
+	int i = 0;
+/**
 	for (i = 0; i < n; ++i)
 	{
 		printf("%d", a[i]);
@@ -21,6 +21,20 @@ void print_array(int *a, int n)
 		{
 			printf("\n");
 		}
+	}
+*/
+	while (i < n)
+	{
+		 printf("%d", a[i]);
+                if (i != n - 1)
+                {
+                        printf(", ");
+                }
+                else
+                {
+                        printf("\n");
+                }
+		i++;
 	}
 
 }
