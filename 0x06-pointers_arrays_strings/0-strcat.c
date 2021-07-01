0406601a79c08f2f9567f83662ffa2a10169757d
abuhanaan <adewunmi99@gmail.com>
2021-07-01 02:40:29 -0700

    Fixed task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,9 +2,9 @@
 #include <string.h>
 
 /**
- * strcat - Conacatenates two strings together
- * dest: First string, second string is appended to it
- * src: Second string, it is appended to the first
+ * _strcat - Conacatenates two strings together
+ * @dest: First string, second string is appended to it
+ * @src: Second string, it is appended to the first
  * Return: dest
  */
 
