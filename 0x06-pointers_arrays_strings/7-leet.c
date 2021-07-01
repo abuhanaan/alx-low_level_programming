7cd6e9927d2f38bd9176bd44b9fee2802a583351
abuhanaan <adewunmi99@gmail.com>
2021-07-01 12:33:22 -0700

    Encodes a string to 1337 format

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * leet - encodes a sting into 1337 format
+ * @str: The string in question
+ * Return: str
+ */
+
+char *leet(char *str)
+{
+	int i, k;
+
+	char s[] = "aAeEoOtTlL";
+	char s1[] = "4433007711";
+
+	while (str[i])
+	{
+		for (k = 0; k <= 9; k++)
+		{
+			if (s[k] == str[i])
+			{
+				str[i] = s1[k];
+			}
+		}
+		i++;
+	}
+	return (str);
+}
