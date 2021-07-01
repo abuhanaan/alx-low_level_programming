37e4e038b3b99d404c4d9d95399403fed9f3ecbf
abuhanaan <adewunmi99@gmail.com>
2021-07-01 10:52:37 -0700

    Converts any lowercase letter of a string to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,14 +11,12 @@
 char *string_toupper(char *str)
 {
 	int i = 0;
-	char c;
 
-	for (i = 0; str[i] != '\0'; ++i)
+	while (str[i])
 	{
-		c = str[i];
-		if (c >= 'a' && c <= 'z')
-			c = c - 32;
-
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
 	}
 
 	return (str);
