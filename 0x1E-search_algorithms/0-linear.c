2f73696b1163d458199d5059ba45370a0846440b
abuhanaan <adewunmi99@gmail.com>
2022-01-19 05:10:42 +0100

    First task

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+/**
+  * linear_search - Searches for a value in an array
+  *                 of integers a using linear search.
+  * @array: A pointer to the first element of the array to be searched.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: -1, if the value is not found or the array is NULL.
+  *         Otherwise, the first index where the value is found.
+  *
+  * Description: Prints a value every time it is compared in the array.
+  */
+
+
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,6 @@
+# 0x1E. C - Search Algorithms
+
+## Resources
+
+- [Search algorithm](https://en.wikipedia.org/wiki/Linear_search)
+- [Space complexity (1)](https://en.wikipedia.org/wiki/Search_algorithm)
