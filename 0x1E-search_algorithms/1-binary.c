a9d4dcabb82736fccec441b08ba6f9fbe47cbd75
abuhanaan <adewunmi99@gmail.com>
2022-01-19 19:43:54 +0100

    task 2-6 - Space and Time compexity

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -16,25 +16,25 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, left, right;
+	size_t i, start, end;
 
 	if (array == NULL)
 		return (-1);
 
-	for (left = 0, right = size - 1; right >= left;)
+	for (start = 0, end = size - 1; end >= start;)
 	{
 		printf("Searching in array: ");
-		for (i = left; i < right; i++)
+		for (i = start; i < end; i++)
 			printf("%d, ", array[i]);
 		printf("%d\n", array[i]);
 
-		i = left + (right - left) / 2;
+		i = start + (end - start) / 2;
 		if (array[i] == value)
 			return (i);
 		if (array[i] > value)
-			right = i - 1;
+			end = i - 1;
 		else
-			left = i + 1;
+			start = i + 1;
 	}
 
 	return (-1);
