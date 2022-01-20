59e196680a2b6218d041b93800c122e664e21012
abuhanaan <adewunmi99@gmail.com>
2022-01-20 04:31:30 +0100

    added prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -27,5 +27,8 @@ int jump_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int advanced_binary_recursive(int *array, size_t start, size_t end, int value);
+int advanced_binary(int *array, size_t size, int value);
+
 
 #endif /* SEARCH_ALGOS_H */
